bef87ae1252b4d70e23314552c9c8350b7c00e71
RoboPatcher24 <108548496+GrootBlouNaai@users.noreply.github.com>
2024-07-06 23:51:51 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,103 +1,88 @@
-import fs from 'fs'
-import path from 'path'
-import tempfile from 'tempfile'
-import mkdirp from 'mkdirp'
-import test from 'ava'
-import del from 'del'
-import steamcmd from './'
+import fs from 'fs';
+import path from 'path';
+import tempfile from 'tempfile';
+import mkdirp from 'mkdirp';
+import test from 'ava';
+import del from 'del';
+import steamcmd from './';
+
+const TEST_DATA_DIR = 'test_data';
 
 test.before(() => {
-  mkdirp.sync('test_data')
-})
+  mkdirp.sync(TEST_DATA_DIR);
+});
 
 test.after.always(() => {
-  return del('test_data')
-})
+  return del(TEST_DATA_DIR);
+});
 
 test.beforeEach(t => {
-  const binDirParent = tempfile('/')
-  mkdirp.sync(binDirParent)
-  const binDir = path.join(binDirParent, 'steamcmd_bin')
-  const opts = {binDir}
-  t.context = {binDirParent, binDir, opts}
-})
+  const binDirParent = tempfile('/');
+  mkdirp.sync(binDirParent);
+  const binDir = path.join(binDirParent, 'steamcmd_bin');
+  t.context = { binDirParent, binDir, opts: { binDir } };
+});
 
 test.afterEach(async t => {
-  await del(t.context.binDirParent, {force: true})
-})
+  await del(t.context.binDirParent, { force: true });
+});
 
-test('download', async t => {
-  var {binDir, opts} = t.context
-  await steamcmd.download(opts)
-  t.notThrows(() => {
-    fs.statSync(binDir)
-  })
-})
+const testDownload = async (t, action) => {
+  const { binDir, opts } = t.context;
+  await action(opts);
+  t.notThrows(() => fs.statSync(binDir));
+};
+
+test('download', testDownload, steamcmd.download);
 
 test('touch', async t => {
-  var {binDir, opts} = t.context
-  await steamcmd.download(opts)
-  // fix random EBUSY on Windows
-  await new Promise(resolve => setTimeout(resolve, 200))
-  await steamcmd.touch(opts)
-  t.notThrows(() => {
-    fs.statSync(path.join(binDir, 'public'))
-  })
-})
+  const { binDir, opts } = t.context;
+  await steamcmd.download(opts);
+  await new Promise(resolve => setTimeout(resolve, 200)); // fix random EBUSY on Windows
+  await steamcmd.touch(opts);
+  t.notThrows(() => fs.statSync(path.join(binDir, 'public')));
+});
 
-test('prep', async t => {
-  var {binDir, opts} = t.context
-  await steamcmd.prep(opts)
-  t.notThrows(() => fs.statSync(path.join(binDir, 'public')))
-})
+test('prep', testDownload, steamcmd.prep);
 
-test('getAppInfo', async t => {
-  var {opts} = t.context
-  await steamcmd.prep(opts)
-  const csgoAppInfo = await steamcmd.getAppInfo(730, opts)
-  t.is(csgoAppInfo.common.name, 'Counter-Strike: Global Offensive')
-  // Ensure the whole text is parsed by checking the last element
-  t.truthy(csgoAppInfo.ufs)
-})
+const testGetAppInfo = async (t, appId, expectedName) => {
+  const { opts } = t.context;
+  await steamcmd.prep(opts);
+  const appInfo = await steamcmd.getAppInfo(appId, opts);
+  t.is(appInfo.common.name, expectedName);
+  t.truthy(appInfo.ufs);
+};
+
+test('getAppInfo', testGetAppInfo, 730, 'Counter-Strike: Global Offensive');
 
 test('repeated calls to getAppInfo', async t => {
-  var {opts} = t.context
-  await steamcmd.prep(opts)
-  const csgoAppInfo = await steamcmd.getAppInfo(730, opts)
-  t.is(csgoAppInfo.common.name, 'Counter-Strike: Global Offensive')
-  t.notThrows(steamcmd.getAppInfo(730, opts))
-})
+  const { opts } = t.context;
+  await steamcmd.prep(opts);
+  const csgoAppInfo = await steamcmd.getAppInfo(730, opts);
+  t.is(csgoAppInfo.common.name, 'Counter-Strike: Global Offensive');
+  t.notThrows(() => steamcmd.getAppInfo(730, opts));
+});
 
-test('updateApp with a relative path', async t => {
-  var {opts} = t.context
-  await steamcmd.prep(opts)
-  t.throws(() => steamcmd.updateApp(1007, 'bad_steamworks', opts))
-  t.throws(() => fs.statSync('bad_steamworks'))
-})
+const testUpdateApp = async (t, appId, installDir, shouldThrow, expectedResult) => {
+  const { opts } = t.context;
+  await steamcmd.prep(opts);
+  if (shouldThrow) {
+    t.throws(() => steamcmd.updateApp(appId, installDir, opts));
+  } else {
+    t.is(await steamcmd.updateApp(appId, installDir, opts), expectedResult);
+  }
+};
 
-test('updateApp with a nonexistent app', async t => {
-  var {opts} = t.context
-  await steamcmd.prep(opts)
-  t.throws(steamcmd.updateApp(4, path.resolve('test_data', 'nonexistent_app'), opts))
-})
-
-test('updateApp with valid parameters', async t => {
-  var {opts} = t.context
-  await steamcmd.prep(opts)
-  t.true(await steamcmd.updateApp(1007, path.resolve('test_data', 'steamworks'), opts))
-})
-
-test('updateApp with HLDS workaround', async t => {
-  var {opts} = t.context
-  await steamcmd.prep(opts)
-  t.true(await steamcmd.updateApp(90, path.resolve('test_data', 'hlds'), opts))
-})
+test('updateApp with a relative path', testUpdateApp, 1007, 'bad_steamworks', true, undefined);
+test('updateApp with a nonexistent app', testUpdateApp, 4, path.resolve(TEST_DATA_DIR, 'nonexistent_app'), true, undefined);
+test('updateApp with valid parameters', testUpdateApp, 1007, path.resolve(TEST_DATA_DIR, 'steamworks'), false, true);
+test('updateApp with HLDS workaround', testUpdateApp, 90, path.resolve(TEST_DATA_DIR, 'hlds'), false, true);
 
 test('updateApp on already up-to-date app returns false', async t => {
-  var {binDirParent, opts} = t.context
-  const appId = 1007
-  const installDir = path.join(binDirParent, 'app')
-  await steamcmd.prep(opts)
-  await steamcmd.updateApp(appId, installDir, opts)
-  t.false(await steamcmd.updateApp(appId, installDir, opts))
-})
+  const { binDirParent, opts } = t.context;
+  const appId = 1007;
+  const installDir = path.join(binDirParent, 'app');
+  await steamcmd.prep(opts);
+  await steamcmd.updateApp(appId, installDir, opts);
+  t.false(await steamcmd.updateApp(appId, installDir, opts));
+});
