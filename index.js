597720a7f7e83e94c88f57af9065f934ba2ee05f
RoboPatcher24 <108548496+GrootBlouNaai@users.noreply.github.com>
2024-07-06 23:45:28 +0200

    Save Changes made from Tinkering on Tulip

--- index.js
+++ index.js
@@ -1,178 +1,171 @@
-/* eslint new-cap: [2, {"capIsNewExceptions": ["extractor.Extract"]}] */
-'use strict'
+'use strict';
 
-var path = require('path')
-var fs = require('fs')
-var Promise = require('pinkie-promise')
-var request = require('request')
-var child = require('child-process-promise')
-var vdf = require('vdf')
+// Import necessary modules
+const path = require('path');
+const fs = require('fs');
+const Promise = require('pinkie-promise');
+const request = require('request');
+const child = require('child-process-promise');
+const vdf = require('vdf');
+const _ = require('lodash.defaults');
+const zlib = require('zlib');
+const unzip = require('unzip');
+const tar = require('tar');
 
-var _ = {}
-_.defaults = require('lodash.defaults')
-
-var defaultOptions = {
+// Define default options for the module
+const defaultOptions = {
   binDir: path.join(__dirname, 'steamcmd_bin')
-}
+};
 
-var download = function (opts) {
-  opts = _.defaults(opts, defaultOptions)
-  var url
-  var extractor
-  if (process.platform === 'win32') {
-    url = 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip'
-    extractor = require('unzip')
-  } else if (process.platform === 'darwin') {
-    url = 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_osx.tar.gz'
-    extractor = require('tar')
-  } else if (process.platform === 'linux') {
-    url = 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz'
-    extractor = require('tar')
-  } else {
-    return Promise.reject('Unsupported platform')
+/**
+ * Determines the URL and extractor based on the current platform.
+ * @returns {Object} - Contains the URL and extractor module, or an error message if the platform is unsupported.
+ */
+const getExtractor = () => {
+  switch (process.platform) {
+    case 'win32':
+      return { url: 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip', extractor: unzip };
+    case 'darwin':
+      return { url: 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_osx.tar.gz', extractor: tar };
+    case 'linux':
+      return { url: 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz', extractor: tar };
+    default:
+      return { error: 'Unsupported platform' };
   }
-  return new Promise(function (resolve, reject) {
-    var req = request(url)
+};
+
+/**
+ * Downloads the SteamCMD installer based on the current platform.
+ * @param {Object} opts - Options object that can override default options.
+ * @returns {Promise} - Resolves when the download and extraction are complete, or rejects with an error.
+ */
+const download = (opts) => {
+  opts = _.defaults(opts, defaultOptions);
+  const { url, extractor, error } = getExtractor();
+  if (error) return Promise.reject(error);
+
+  return new Promise((resolve, reject) => {
+    let req = request(url);
     if (process.platform !== 'win32') {
-      req = req.pipe(require('zlib').createGunzip())
+      req = req.pipe(zlib.createGunzip());
     }
-    req.pipe(extractor.Extract({path: opts.binDir})
+    req.pipe(extractor.Extract({ path: opts.binDir }))
       .on('finish', resolve)
-      .on('error', reject)
-    )
-  })
-}
+      .on('error', reject);
+  });
+};
 
-var downloadIfNeeded = function (opts) {
-  opts = _.defaults(opts, defaultOptions)
+/**
+ * Checks if the SteamCMD binary directory exists and downloads it if it doesn't.
+ * @param {Object} opts - Options object that can override default options.
+ * @returns {Promise} - Resolves if the directory exists, or after downloading and extracting if it doesn't.
+ */
+const downloadIfNeeded = (opts) => {
+  opts = _.defaults(opts, defaultOptions);
   try {
-    fs.statSync(opts.binDir)
-    return Promise.resolve()
+    fs.statSync(opts.binDir);
+    return Promise.resolve();
   } catch (err) {
-    return download(opts)
+    return download(opts);
   }
-}
+};
 
-var run = function (commands, opts) {
-  opts = _.defaults(opts, defaultOptions)
-  var exeName
-  if (process.platform === 'win32') {
-    exeName = 'steamcmd.exe'
-  } else if (process.platform === 'darwin') {
-    exeName = 'steamcmd.sh'
-  } else if (process.platform === 'linux') {
-    exeName = 'steamcmd.sh'
-  } else {
-    return Promise.reject('Unsupported platform')
-  }
-  var args = commands.concat('quit').map(function (x) {
-    return '+' + x
-  }).join(' ').split(' ')
-  return new Promise(function (resolve, reject) {
-    child.spawn(path.join(opts.binDir, exeName),
-      args,
-      {
-        capture: ['stdout', 'stderr'],
-        cwd: opts.binDir
-      }
-    ).then(function (x) {
-      resolve(x)
-    }).fail(function (x) {
-      // For some reason, steamcmd will occasionally exit with code 7 and be fine.
-      // This usually happens the first time touch() is called after download().
-      if (x.code === 7) {
-        resolve(x)
-      } else {
-        reject(x)
-      }
-    })
-  })
-}
+/**
+ * Runs SteamCMD with the given commands.
+ * @param {Array} commands - Array of commands to pass to SteamCMD.
+ * @param {Object} opts - Options object that can override default options.
+ * @returns {Promise} - Resolves with the process output or rejects with an error.
+ */
+const run = (commands, opts) => {
+  opts = _.defaults(opts, defaultOptions);
+  const exeName = process.platform === 'win32' ? 'steamcmd.exe' : 'steamcmd.sh';
+  if (!exeName) return Promise.reject('Unsupported platform');
 
-var touch = function (opts) {
-  opts = _.defaults(opts, defaultOptions)
-  return run([], opts)
-}
+  const args = commands.concat('quit').map(x => '+' + x).join(' ').split(' ');
+  return new Promise((resolve, reject) => {
+    child.spawn(path.join(opts.binDir, exeName), args, { capture: ['stdout', 'stderr'], cwd: opts.binDir })
+      .then(resolve)
+      .catch(x => {
+        if (x.code === 7) resolve(x);
+        else reject(x);
+      });
+  });
+};
 
-var getAppInfo = function (appID, opts) {
-  opts = _.defaults(opts, defaultOptions)
+/**
+ * Touches the SteamCMD executable to ensure it's ready to run.
+ * @param {Object} opts - Options object that can override default options.
+ * @returns {Promise} - Resolves after touching the SteamCMD executable.
+ */
+const touch = (opts) => {
+  opts = _.defaults(opts, defaultOptions);
+  return run([], opts);
+};
 
-  // The first call to app_info_print from a new install will return nothing,
-  // and it will instead prep an entry for the info and request it.
-  // It won't block though, and if the session closes before it can save,
-  // the same issue will be present on next run.
-  // Thus we use `app_update` to force the session to wait long enough to save.
-  var forceInfoCommand = [
-    '@ShutdownOnFailedCommand 0', 'login anonymous',
-    'app_info_print ' + appID,
-    'force_install_dir ./4', 'app_update 4'
-  ]
+/**
+ * Retrieves application information for a given app ID.
+ * @param {Number} appID - The application ID to retrieve information for.
+ * @param {Object} opts - Options object that can override default options.
+ * @returns {Promise} - Resolves with the parsed application information or rejects with an error.
+ */
+const getAppInfo = (appID, opts) => {
+  opts = _.defaults(opts, defaultOptions);
+  const forceInfoCommand = ['@ShutdownOnFailedCommand 0', 'login anonymous', `app_info_print ${appID}`, 'force_install_dir ./4', 'app_update 4'];
+  const fetchInfoCommand = ['@ShutdownOnFailedCommand 0', 'login anonymous', 'app_info_update 1', `app_info_print ${appID}`, 'find e'];
 
-  // The output from app_update can collide with that of app_info_print,
-  // so after ensuring the info is available we must re-run without app_update.
-  var fetchInfoCommand = [
-    '@ShutdownOnFailedCommand 0', 'login anonymous',
-    'app_info_update 1', // force data update
-    'app_info_print ' + appID,
-    'find e' // fill the buffer so info's not truncated on Windows
-  ]
+  return run(forceInfoCommand, opts)
+    .then(() => run(fetchInfoCommand, opts))
+    .then(proc => {
+      let stdout = proc.stdout.replace('\r\n', '\n');
+      const infoTextStart = stdout.indexOf(`"${appID}"`);
+      const infoTextEnd = stdout.indexOf('ConVars:');
+      const infoText = stdout.substr(infoTextStart, infoTextEnd - infoTextStart);
+      return vdf.parse(infoText)[appID];
+    });
+};
 
-  return run(forceInfoCommand, opts) // @todo only force when needed
-    .then(function () {
-      return run(fetchInfoCommand, opts)
-    })
-    .then(function (proc) {
-      // strip Windows line endings
-      var stdout = proc.stdout.replace('\r\n', '\n')
-      // extract & parse info
-      var infoTextStart = stdout.indexOf('"' + appID + '"')
-      var infoTextEnd = stdout.indexOf('ConVars:')
-      var infoText = stdout.substr(infoTextStart, infoTextEnd - infoTextStart)
-      return vdf.parse(infoText)[appID]
-    })
-}
+/**
+ * Updates a Steam application to the latest version.
+ * @param {Number} appId - The application ID to update.
+ * @param {String} installDir - The installation directory for the application.
+ * @param {Object} opts - Options object that can override default options.
+ * @returns {Promise} - Resolves with a boolean indicating if the application was updated or rejects with an error.
+ */
+const updateApp = (appId, installDir, opts) => {
+  opts = _.defaults(opts, defaultOptions);
+  if (!path.isAbsolute(installDir)) throw new TypeError('installDir must be an absolute path in updateApp');
 
-var updateApp = function (appId, installDir, opts) {
-  opts = _.defaults(opts, defaultOptions)
-  if (!path.isAbsolute(installDir)) {
-    // throw an error immediately because it's invalid data, not a failure
-    throw new TypeError('installDir must be an absolute path in updateApp')
-  }
-  var commands = ['@ShutdownOnFailedCommand 0', 'login anonymous', 'force_install_dir ' + installDir, 'app_update ' + appId]
-  if (parseInt(appId, 10) === 90) {
-    commands = commands.concat('app_update ' + appId)
-  }
+  const commands = ['@ShutdownOnFailedCommand 0', 'login anonymous', `force_install_dir ${installDir}`, `app_update ${appId}`];
+  if (parseInt(appId, 10) === 90) commands.push(`app_update ${appId}`);
+
   return run(commands, opts)
-    .then(function (proc) {
-      if (proc.stdout.indexOf('Success! App \'' + appId + '\' fully installed') !== -1) {
-        return true
-      }
-      if (proc.stdout.indexOf('Success! App \'' + appId + '\' already up to date.') !== -1) {
-        return false
-      }
+    .then(proc => {
+      const stdout = proc.stdout.replace('\r\n', '\n');
+      if (stdout.includes(`Success! App '${appId}' fully installed`)) return true;
+      if (stdout.includes(`Success! App '${appId}' already up to date.`)) return false;
 
-      var stdoutArray = proc.stdout.replace('\r\n', '\n').split('\n')
-      return Promise.reject(new Error('Unable to update ' + appId + '. \n SteamCMD error was ' + stdoutArray[stdoutArray.length - 2]))
-    })
-}
+      const stdoutArray = stdout.split('\n');
+      return Promise.reject(new Error(`Unable to update ${appId}. \n SteamCMD error was ${stdoutArray[stdoutArray.length - 2]}`));
+    });
+};
 
-var prep = function (opts) {
-  opts = _.defaults(opts, defaultOptions)
+/**
+ * Prepares the SteamCMD environment by ensuring the binaries are downloaded and ready.
+ * @param {Object} opts - Options object that can override default options.
+ * @returns {Promise} - Resolves after preparing the environment.
+ */
+const prep = (opts) => {
+  opts = _.defaults(opts, defaultOptions);
   return downloadIfNeeded(opts)
-    .then(function () {
-      return new Promise(function (resolve) {
-        setTimeout(resolve, 500)
-      })
-    })
-    .then(function () {
-      return touch(opts)
-    })
-}
+    .then(() => new Promise(resolve => setTimeout(resolve, 500)))
+    .then(() => touch(opts));
+};
 
-module.exports = {}
-
-module.exports.download = downloadIfNeeded
-module.exports.touch = touch
-module.exports.prep = prep
-module.exports.getAppInfo = getAppInfo
-module.exports.updateApp = updateApp
+// Export the functions for use in other modules
+module.exports = {
+  download: downloadIfNeeded,
+  touch,
+  prep,
+  getAppInfo,
+  updateApp
+};
